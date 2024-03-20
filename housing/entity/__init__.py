49610f0173a074c8cec208ee4c25e6f515ce2923
Sid1902 <siddhantbedmutha111@gmail.com>
2024-03-20 15:51:29 +0530

    updated flow_digrams

---
+++ housing/entity/__init__.py
@@ -0,0 +1,19 @@
+# in entity we will find the artifact for each component 
+# Artifact is defined for each component of pipeline
+""" 
+Diiferent artifacts for this project will be :
+DataIngestion Artifact
+DataValidation Artifact
+DataTranformation Artifact
+ModelTrainer Artifact
+ModelEvaluation Artifact
+ModelPusher Artifact
+
+DataIngestion Config
+DataValidation Config
+DataTranformation Config
+ModelTrainer Config
+ModelEvaluation Config
+ModelPusher Config
+
+"""
